4f9acaaeba0eba77c192eafe76d63dede93d661f
jacquelineschmitz <jacquelineschmitz81@gmail.com>
2022-11-29 11:24:21 +0100

    ...

--- VorzeigeProjekt/MainViewController.swift
+++ VorzeigeProjekt/MainViewController.swift
@@ -8,14 +8,36 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    @IBOutlet weak var loginBTN: UIButton!
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    var isValidLogin = false
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginBTN.isEnabled = false
+        usernameTF.delegate = self
+        passwordTF.delegate = self
+        
 
     }
     
     @IBAction func loginPressed(){
-        performSegue(withIdentifier: "startToTabViewSegue", sender: self)
+        if(isValidLogin){
+              performSegue(withIdentifier: "startToTabViewSegue", sender: self)
+            } else {
+              let alert = UIAlertController(title: "LogIn failed", message: "Please try again", preferredStyle: .alert)
+              alert.addAction(UIAlertAction(title: "try again", style: .cancel, handler: {_ in
+                self.usernameTF.text = ""
+                self.passwordTF.text = ""
+                self.loginBTN.isEnabled = false
+                alert.dismiss(animated: true)
+              }))
+              present (alert, animated: true)
+            }
     }
     
     @IBAction func registerPressed(){
@@ -23,4 +45,21 @@ class MainViewController: UIViewController {
     }
     
 
+}
+extension MainViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return true
+    }
+    
+    @IBAction func onTextChanged(){
+        if(usernameTF.text?.count ?? 0 > 0 && passwordTF.text?.count ?? 0 > 0){
+              loginBTN.isEnabled = true
+              if(usernameTF.text == "Jacky" && passwordTF.text == "1234"){
+                isValidLogin = true
+              } else {
+                isValidLogin = false
+              }
+            }
+    }
 }
